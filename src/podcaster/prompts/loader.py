b3b7e9d16d1ac7d935b353bb128d84b6d555c544
Mikołaj Dawczyk <mikolaj.dawczyk@gmail.com>
2025-12-22 21:51:36 +0100

    style: formated loader.py with ruff

--- src/podcaster/prompts/loader.py
+++ src/podcaster/prompts/loader.py
@@ -1,11 +1,14 @@
 from models.configs import PromptConfig
 
+
 def load_story_prompt() -> str:
-    """Load the story prompt template from a file."""
+    """Load the prompt template from a file and format it with length and language."""
     prompt_config = PromptConfig()
     template_path = prompt_config.PROMPT_TEMPLATE_PATH
     prompt_template = ""
     with open(template_path, "r", encoding="utf-8") as file:
         prompt_template = file.read()
-    prompt = prompt_template.format(length=prompt_config.LENGTH_MINUTES, language=prompt_config.LANGUAGE)
+    prompt = prompt_template.format(
+        length=prompt_config.LENGTH_MINUTES, language=prompt_config.LANGUAGE
+    )
     return prompt
