6e54fd595e5458020841d37c8caf29602a05724b
Mikołaj Dawczyk <mikolaj.dawczyk@gmail.com>
2025-12-22 19:07:26 +0100

    feat: splited prompt and llm config

--- src/podcaster/models/configs.py
+++ src/podcaster/models/configs.py
@@ -6,10 +6,6 @@ class LLMConfig(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     OPENAI_API_KEY: str = Field(..., alias="OPENAI_API_KEY")
     OPENAI_MODEL: str = Field(..., alias="OPENAI_MODEL")
-    LANGUAGE: str = Field(..., alias="SCRIPT_LANGUAGE")
-    SCRIPT_LENGTH_LETTERS_MIN: int = Field(..., alias="SCRIPT_LENGTH_LETTERS_MIN")
-    SCRIPT_LENGTH_LETTERS_MAX: int = Field(..., alias="SCRIPT_LENGTH_LETTERS_MAX")
-    SCRIPT_LENGTH_MINUTES: int = Field(..., alias="SCRIPT_LENGTH_MINUTES")
 
 
 class TTSConfig(BaseSettings):
@@ -18,3 +14,12 @@ class TTSConfig(BaseSettings):
     ELEVENLABS_MALE_VOICE_ID: str = Field(..., alias="ELEVENLABS_MALE_VOICE_ID")
     ELEVENLABS_FEMALE_VOICE_ID: str = Field(..., alias="ELEVENLABS_FEMALE_VOICE_ID")
     ELEVENLABS_SPEED: float = Field(..., alias="ELEVENLABS_SPEED")
+
+
+class PromptConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    STORY_PROMPT_TEMPLATE_PATH: str = Field(..., alias="STORY_PROMPT_TEMPLATE_PATH")
+    STORY_LENGTH_LETTERS_MIN: int = Field(..., alias="STORY_LENGTH_LETTERS_MIN")
+    STORY_LENGTH_LETTERS_MAX: int = Field(..., alias="STORY_LENGTH_LETTERS_MAX")
+    STORY_LENGTH_MINUTES: int = Field(..., alias="STORY_LENGTH_MINUTES")
+    LANGUAGE: str = Field(..., alias="STORY_LANGUAGE")
